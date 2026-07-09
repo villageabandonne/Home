2de8d4f3b8beb04344e4157b8f6ed413181018c1
saarzhanova <alinasarzhanova@gmail.com>
2026-07-10 01:31:06 +0200

    clean up

--- hscroll.js
+++ hscroll.js
@@ -3,7 +3,21 @@ const artPage = document.getElementById('artPage');
 const pages = document.querySelectorAll('.h-page');
 const btnTech = document.getElementById('tech');
 const btnArt = document.getElementById('art');
+const btnArtBack = document.getElementById('artBack');
 const btnBack = document.querySelectorAll('.back');
+
+const scrollContainer = document.getElementById('scrollContainer');
+function lockVerticalScroll() {
+    scrollContainer.style.overflowY = 'hidden';
+    document.body.style.overflowY = 'hidden';
+    document.documentElement.style.overflowY = 'hidden';
+}
+
+function unlockVerticalScroll() {
+    scrollContainer.style.overflowY = 'auto';
+    document.body.style.overflowY = 'auto';
+    document.documentElement.style.overflowY = 'auto';
+}
 
 let index = 1;
 
@@ -20,17 +34,26 @@ function goTo(i) {
 let to = 0
 
 btnArt.addEventListener('click', () => {
+    console.log('Art');
     to = -1;
     goTo(index + to);
+    lockVerticalScroll();
 });
 
 btnTech.addEventListener('click', () => {
     to = 1;
     goTo(index + to);
+    lockVerticalScroll();
 });
 
-btnBack.forEach(btn => {
-    btn.addEventListener('click', () => {
-        goTo(index - to);
-    });
+btnArtBack.addEventListener('click', () => {
+    console.log('back');
+    goTo(1);
+    unlockVerticalScroll();
 });
+
+// btnBack.forEach(btn => {
+//     btn.addEventListener('click', () => {
+//         goTo(index - to);
+//     });
+// });
